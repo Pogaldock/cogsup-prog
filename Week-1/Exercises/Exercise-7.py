ed3c2512644d21c454eba70deabf8a09f5e12cec
Rowan Kearney <rowlunch@gmail.com>
2025-09-21 11:23:54 +0200

    continued the exercises

--- Week-1/Exercises/Exercise-7.py
+++ Week-1/Exercises/Exercise-7.py
@@ -8,3 +8,41 @@ At every step, add comments reflecting the logic of what the particular line of 
 to be) doing. 
 """
 
+
+
+def guess_number():
+
+    # Set initial parameters
+    low = 1
+    high = 100
+    found = False
+
+    # Keep guessing untill correct number is found
+    while not found:
+
+        # Take the middle of the current search space as a guess
+        guess = (low+high)//2
+
+        # Ask user for feedback
+        response = input(f"Is {guess} the number you are thinking of? Hit '+' if your number is higher, '-' if lower, or press Enter if my guess is correct: ")
+        
+        # Use user feedback to redefine current search space
+        if response == "":
+            print(f"I win! Your number was {guess}")
+            # This ends the game
+            found = True
+
+        elif response == "+":
+            # We know the number is between the max and the previous guess so we redefine the search space
+            low = guess + 1
+
+        elif response == "-":
+            # We know the number is between the min and the previous guess so we redefine the search space
+            high = guess - 1
+
+        else:
+            # Catches wrong inputs
+            print("Invalid response.")
+
+# run the game
+guess_number()
--- Week-2/Exercises/launching_function.py
+++ Week-2/Exercises/launching_function.py
@@ -1,6 +1,6 @@
 from expyriment import design, control, stimuli
 
-def run_launching_sequence(temporal_gap_ms=200, spatial_gap_px=80, speedup_factor=3.0):
+def run_launching_experiments(temporal_gap_ms=200, spatial_gap_px=80, speedup_factor=3.0):
     control.defaults.initialise_delay = 0
     control.defaults.window_mode = True
     control.defaults.fast_quit = True
@@ -9,14 +9,15 @@ def run_launching_sequence(temporal_gap_ms=200, spatial_gap_px=80, speedup_facto
     control.initialize(exp)
     control.start(subject_id=1)
 
+    # Initial parameters
     SIZE = 50
     RED_START = -400
     CENTER = 0
     GREEN_START = CENTER
     GREEN_END = 400
     RED_TOUCH = CENTER - SIZE
-    step = 6
-    frame = 16
+    step = 5
+    frame = 20
 
     def draw(r, g):
         g.present(clear=True, update=False)
@@ -30,36 +31,47 @@ def run_launching_sequence(temporal_gap_ms=200, spatial_gap_px=80, speedup_facto
             draw(r=sq, g=other)
             exp.clock.wait(frame)
 
-    def fresh():
-        r = stimuli.Rectangle((SIZE, SIZE), "red", (RED_START, 0))
-        g = stimuli.Rectangle((SIZE, SIZE), "green", (GREEN_START, 0))
+    def reset_pos():
+        r = stimuli.Rectangle((SIZE, SIZE), colour="red",   position=(RED_START, 0))
+        g = stimuli.Rectangle((SIZE, SIZE), colour="green", position=(GREEN_START, 0))
         draw(r, g)
         exp.clock.wait(250)
         return r, g
+    
+    def show_label(text):
+        stimuli.TextLine(text, position=(0, 0)).present(clear=True, update=True)
+        exp.clock.wait(600)
 
-    # 1: Michotte
-    red, green = fresh()
+    # Michotte
+    show_label("Michottean launching")
+    red, green = reset_pos()
     move_x(red, RED_TOUCH, step, green)
     move_x(green, GREEN_END, step, red)
     exp.clock.wait(300)
 
-    # 2: Temporal gap
-    red, green = fresh()
+    # Temporal gap
+    show_label("Temporal gap")
+    red, green = reset_pos()
     move_x(red, RED_TOUCH, step, green)
     exp.clock.wait(temporal_gap_ms)
     move_x(green, GREEN_END, step, red)
     exp.clock.wait(300)
 
-    # 3: Spatial gap
-    red, green = fresh()
+    # Spatial gap
+    show_label("Spatial gap")
+    red, green = reset_pos()
     move_x(red, RED_TOUCH - spatial_gap_px, step, green)
     move_x(green, GREEN_END, step, red)
     exp.clock.wait(300)
 
-    # 4: Triggering
-    red, green = fresh()
+    # Triggering
+    show_label("Triggering")
+    red, green = reset_pos()
     move_x(red, RED_TOUCH, step, green)
     move_x(green, GREEN_END, int(step * speedup_factor), red)
 
+    stimuli.TextLine("Press any key to exit.").present(clear=True, update=True)
     exp.keyboard.wait()
     control.end()
+
+run_launching_experiments(1000,100,5)
